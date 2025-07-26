9bcacb0f9f576821a8005786a8ef5e378d4a2332
Magdaleno Martínez Unzueta <iscmagdaleno@gmail.com>
2025-07-25 19:08:54 -0600

    Se mejora proceso al pagar articulos

--- BaseDeDatos/Procedures/spSaveAbonoApartado.sql
+++ BaseDeDatos/Procedures/spSaveAbonoApartado.sql
@@ -1,82 +1,119 @@
 IF OBJECT_ID( 'spSaveAbonoApartado' ) IS NULL
 	EXEC ('CREATE PROCEDURE spSaveAbonoApartado AS SET NOCOUNT ON;') 
 GO 
-ALTER PROCEDURE spSaveAbonoApartado (
-@iIdAbonoApartado INT, 
-@iIdApartado INT, 
-@mAbono MONEY
-) 
-AS 
-BEGIN 
+ALTER PROCEDURE dbo.spSaveAbonoApartado
+(
+    @iIdAbonoApartado INT = NULL OUTPUT,
+    @iIdApartado      INT,
+    @mAbono           MONEY
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SET XACT_ABORT ON;
 
-DECLARE @vchError VARCHAR(200) = '';
+    DECLARE @vchError          VARCHAR(200)  = '';
+    DECLARE @mTotalPagar       MONEY;
+    DECLARE @mTotalAbonado     MONEY;
+    DECLARE @dtFechaVenta      DATETIME      = GETDATE();
+    DECLARE @vchReferenciaVenta VARCHAR(100);
 
-DECLARE @Result AS TABLE (
-	bResult BIT DEFAULT(1),
-	vchMessage VARCHAR(500) DEFAULT(''),
-	iIdAbonoApartado INT DEFAULT( -1 ) 
-	);
+    -- Tabla de resultado estructurado
+    DECLARE @Result TABLE (
+        bResult            BIT            DEFAULT (1),
+        vchMessage         VARCHAR(500)   DEFAULT (''),
+        iIdAbonoApartado   INT            DEFAULT (-1)
+    );
 
-SET NOCOUNT ON
-SET XACT_ABORT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
 
-BEGIN TRY
+        -- Insertar el nuevo abono
+        INSERT INTO dbo.AbonoApartado (iIdApartado, mAbono, dtFechaAbono)
+        VALUES (@iIdApartado, @mAbono, @dtFechaVenta);
 
-BEGIN TRANSACTION
+        SET @iIdAbonoApartado = SCOPE_IDENTITY();
 
+        -- Obtener el total a pagar y el total abonado
+        SELECT @mTotalPagar = mTotal
+        FROM dbo.Apartado
+        WHERE iIdApartado = @iIdApartado;
 
-	INSERT INTO dbo.AbonoApartado
-	 ( 
+        SELECT @mTotalAbonado = SUM(mAbono)
+        FROM dbo.AbonoApartado
+        WHERE iIdApartado = @iIdApartado;
 
-		iIdApartado, 			mAbono, 			dtFechaAbono 	
+        SET @vchReferenciaVenta = CONCAT('Apartado #', @iIdApartado);
 
-	)
-	VALUES 
-	(
-		@iIdApartado,		@mAbono,		GETDATE()
+        -- Si se liquidó el apartado...
+        IF (@mTotalAbonado >= @mTotalPagar)
+        BEGIN
+            -- Marcar apartado como pagado
+            UPDATE dbo.Apartado
+            SET bPagado = 1
+            WHERE iIdApartado = @iIdApartado;
 
-	)
-		 SET @iIdAbonoApartado = @@IDENTITY
+            -- Insertar los artículos en la tabla Venta
+            INSERT INTO dbo.Venta (
+                iIdArticulo,
+                iCantidad,
+                mPrecioFinal,
+                dtFechaVenta,
+                vchReferenciaVenta
+            )
+            SELECT
+                ad.iIdArticulo,
+                ad.iCantidad,
+                ad.mPrecioFinal,
+                @dtFechaVenta,
+                @vchReferenciaVenta
+            FROM dbo.ApartadoDetalle ad
+            WHERE ad.iIdApartado = @iIdApartado;
 
+            -- Actualizar Inventario (quitar de smApartados la cantidad vendida)
+            UPDATE inv
+            SET inv.smApartados = 
+                CASE 
+                    WHEN inv.smApartados >= ad.iCantidad THEN inv.smApartados - ad.iCantidad
+                    ELSE 0
+                END
+            FROM dbo.Inventario inv
+            INNER JOIN dbo.ApartadoDetalle ad
+                ON inv.iIdArticulo = ad.iIdArticulo
+            WHERE ad.iIdApartado = @iIdApartado;
+        END
 
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
 
-		COMMIT TRANSACTION ;
-	END TRY
-	BEGIN CATCH
-		ROLLBACK TRANSACTION ;
+        SET @vchError = CONCAT(
+            'spSaveAbonoApartado: ',
+            ERROR_MESSAGE(),
+            ' ',
+            ERROR_PROCEDURE(),
+            ' ',
+            ERROR_LINE()
+        );
+        PRINT @vchError;
+    END CATCH;
 
-		SELECT @vchError = CONCAT( 'spSaveAbonoApartado: ', ERROR_MESSAGE( ), ' ', ERROR_PROCEDURE( ), ' ', ERROR_LINE( ) );
-		PRINT @vchError;
-	END CATCH
+    -- Registrar resultado final
+    IF LEN(@vchError) > 0
+    BEGIN
+        INSERT INTO @Result (bResult, vchMessage)
+        VALUES (0, @vchError);
+    END
+    ELSE
+    BEGIN
+        INSERT INTO @Result (bResult, vchMessage, iIdAbonoApartado)
+        VALUES (1, '', @iIdAbonoApartado);
+    END
 
-	IF LEN( @vchError ) > 0
-	BEGIN
-		INSERT INTO @Result
-		(
-			bResult,vchMessage
-		)
-		VALUES
-		(
-			0,@vchError
-		);
-	END
-	ELSE
-	BEGIN
-		INSERT INTO @Result
-		(
-			bResult,vchMessage,iIdAbonoApartado
-		)
-		VALUES
-		(
-			1,'',@iIdAbonoApartado
-		);
-	END;
+    SELECT *
+    FROM @Result;
 
-	SELECT *
-	FROM
-		@Result;
-	SET NOCOUNT OFF;
-END;
-GO 
-
-
+    SET NOCOUNT OFF;
+END
+GO
--- BaseDeDatos/Procedures/spSaveApartado.sql
+++ BaseDeDatos/Procedures/spSaveApartado.sql
@@ -1,49 +1,48 @@
-IF OBJECT_ID('dbo.spSaveApartado', 'P') IS NOT NULL
-    DROP PROCEDURE dbo.spSaveApartado;
-GO
-
-CREATE PROCEDURE dbo.spSaveApartado
+IF OBJECT_ID( 'spSaveApartado' ) IS NULL
+	EXEC ('CREATE PROCEDURE spSaveApartado AS SET NOCOUNT ON;') 
+GO 
+ALTER PROCEDURE dbo.spSaveApartado
     @nvchNombreCliente NVARCHAR(400), 
     @mTotal            MONEY, 
     @bPagado           BIT, 
     @nvchComentario    NVARCHAR(2000),
-    @Detalles          dbo.DTApartadoDetalle READONLY -- Nuevo parámetro de tipo tabla
+    @Detalles          dbo.DTApartadoDetalle READONLY -- Tabla de detalles recibida
 AS
 BEGIN
     SET NOCOUNT ON;
     SET XACT_ABORT ON;
 
-    DECLARE @vchError VARCHAR(200) = '',
-    @iIdApartado INT;
+    DECLARE @vchError    VARCHAR(200) = '';
+    DECLARE @iIdApartado INT;
 
     DECLARE @Result AS TABLE (
         bResult     BIT            DEFAULT (1),
-        vchMessage  VARCHAR(500)   DEFAULT (''),
-        iIdApartado INT           DEFAULT (-1)
+        vchMessage  VARCHAR(500)   DEFAULT (''), -- Resultado operativo o error
+        iIdApartado INT            DEFAULT (-1)
     );
 
     BEGIN TRY
         BEGIN TRANSACTION;
 
+        -- Insertar registro en Apartado
+        INSERT INTO dbo.Apartado (
+            nvchNombreCliente,
+            dtFechaApartado,
+            mTotal,
+            bPagado,
+            nvchComentario
+        )
+        VALUES (
+            @nvchNombreCliente,
+            GETDATE(),
+            @mTotal,
+            @bPagado,
+            @nvchComentario
+        );
 
-            INSERT INTO dbo.Apartado (
-                nvchNombreCliente,
-                dtFechaApartado,
-                mTotal,
-                bPagado,
-                nvchComentario
-            )
-            VALUES (
-                @nvchNombreCliente,
-                GETDATE(),
-                @mTotal,
-                @bPagado,
-                @nvchComentario
-            );
+        SET @iIdApartado = SCOPE_IDENTITY();
 
-            SET @iIdApartado = SCOPE_IDENTITY();
-    
-        -- Insertar detalles en la tabla ApartadoDetalle, forzando la relación al Apartado
+        -- Insertar detalles en ApartadoDetalle, referenciando el Id del nuevo Apartado
         INSERT INTO dbo.ApartadoDetalle (
             iIdApartado,
             iIdArticulo,
@@ -51,21 +50,42 @@ BEGIN
             mPrecioFinal
         )
         SELECT
-            @iIdApartado,           -- Relacionar al Apartado 'padre'
+            @iIdApartado,
             d.iIdArticulo,
             d.iCantidad,
             d.mPrecioFinal
         FROM @Detalles d;
 
+        /*
+            Actualizar Inventario:
+            - Restar iCantidad de smCantidad
+            - Sumar iCantidad a smApartados
+        */
+        UPDATE inv
+        SET 
+            inv.smCantidad   = inv.smCantidad  - det.iCantidad,
+            inv.smApartados  = inv.smApartados + det.iCantidad
+        FROM 
+            dbo.Inventario inv
+            INNER JOIN @Detalles det ON inv.iIdArticulo = det.iIdArticulo
+        WHERE 
+            -- Solo actualizar si hay suficiente cantidad
+            inv.smCantidad >= det.iCantidad;
+
+        /*
+            Opcional: Puedes manejar faltante/inventario insuficiente aquí.
+            Ejemplo: si alguna fila no se actualizó por cantidad insuficiente, lanzar error.
+        */
+        
+
         COMMIT TRANSACTION;
     END TRY
     BEGIN CATCH
         ROLLBACK TRANSACTION;
-
-        SET @vchError = CONCAT('spSaveApartado: ', ERROR_MESSAGE(), ' ', ERROR_PROCEDURE(), ' ', ERROR_LINE());
+        SET @vchError = CONCAT('spSaveApartado: ', ERROR_MESSAGE(), ' en ', ERROR_PROCEDURE(), ' línea ', ERROR_LINE());
     END CATCH
 
-    -- Resultados según éxito o error
+    -- Indicar resultado final
     IF LEN(@vchError) > 0
     BEGIN
         INSERT INTO @Result (bResult, vchMessage)
@@ -82,4 +102,5 @@ BEGIN
 
     SET NOCOUNT OFF;
 END
+
 GO
